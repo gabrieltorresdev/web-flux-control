d7b6f403317db9b1cde6f63879f8fb77d3bbfed8
Gabriel Torres <dev.gabriel.torres@gmail.com>
2024-12-12 17:23:44 -0300

    feat: update transaction list skeleton's style

--- src/components/transactions/transaction-skeleton.tsx
+++ src/components/transactions/transaction-skeleton.tsx
@@ -1,16 +1,29 @@
-"use client";
-
-import React, { memo } from "react";
+import { memo } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function TransactionSkeletonComponent() {
   return (
-    <div className="flex items-center gap-2 p-3 bg-white animate-pulse">
-      <div className="w-10 h-10 rounded-full bg-gray-200" />
-      <div className="flex-1">
-        <div className="h-4 w-2/3 bg-gray-200 rounded mb-2" />
-        <div className="h-3 w-1/3 bg-gray-200 rounded" />
+    <div className="flex flex-col gap-1.5">
+      <div className="flex items-center justify-between p-3 pt-0">
+        <Skeleton className="h-4 w-36 self-end" />
+        <Skeleton className="w-6 h-6 rounded-full" />
       </div>
-      <div className="h-4 w-24 bg-gray-200 rounded" />
+      <div className="flex flex-col gap-3">
+        {Array.from({
+          length: 2,
+        }).map((_, index) => (
+          <div
+            key={`transaction-skeleton-${index}`}
+            className="flex items-center gap-3 p-3"
+          >
+            <Skeleton className="w-8 h-8 rounded-full" />
+            <div className="flex-1">
+              <Skeleton className="h-4 w-52 mb-2" />
+              <Skeleton className="h-3 w-32" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
